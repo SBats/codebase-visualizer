f897a63b9579ff9fb0fede0261027e8914b5617d
Simon Bats <simon.bats12@gmail.com>
2021-08-11 21:11:01 -0400

    Update script to read from folder path recursively

--- get_html_children.ts
+++ get_html_children.ts
@@ -3,11 +3,27 @@ import { readSync } from 'to-vfile'
 import rehypeParse from 'rehype-parse'
 import { Parent, Root, Element } from 'hast';
 import { ValuesType } from 'utility-types';
+import { readdirSync } from 'fs';
+import path from 'path';
 
 const scriptArguments = process.argv.slice(2);
-const filePath = scriptArguments[0];
+const parentFolderPath = scriptArguments[0];
 
-type flattenNode = { tagName: String; className: String };
+type flattenNode = { tagName: string; className: string };
+
+function isHTMLFile(fileName: string): boolean {
+  return fileName.split('.').reverse()[0] === 'html';
+}
+
+function getAllHTMLFiles(folderPath: string): string[] {
+  const entries = readdirSync(folderPath, { withFileTypes: true });
+  return entries.flatMap(entry => {
+    const filePath = path.resolve(folderPath, entry.name);
+    if (entry.isDirectory()) return getAllHTMLFiles(filePath);
+    if (isHTMLFile(entry.name)) return filePath;
+    return [];
+  })
+}
 
 function simplifyChild(node: Element): flattenNode {
   return { tagName: node.tagName, className: JSON.stringify(node.properties?.className) }
@@ -24,13 +40,20 @@ function getFlatenTreeChildren(tree: Root): Array<flattenNode> {
 
 function main() {
   try {
-      const htmlFile = readSync(filePath)
+      const htmlFiles = getAllHTMLFiles(parentFolderPath);
 
-      const ast = unified()
-        .use(rehypeParse, { fragment: true })
-        .parse(htmlFile)
+      const children = htmlFiles.map(filePath => {
+        const htmlFile = readSync(filePath)
 
-      console.log(getFlatenTreeChildren(ast));
+        const ast = unified()
+          .use(rehypeParse, { fragment: true })
+          .parse(htmlFile)
+
+        return getFlatenTreeChildren(ast)
+      })
+
+      console.log(htmlFiles);
+      console.log(children);
   } catch (e) {
       console.error(e);
   }
