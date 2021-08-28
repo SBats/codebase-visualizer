bcc80dc42273714ba7ecfcf1cc3b6575d48ed8e9
Simon Bats <simon.bats12@gmail.com>
2021-08-27 22:00:44 -0400

    Move ng1 methods into their own module

--- get_ng1_components.ts
+++ get_ng1_components.ts
@@ -1,130 +1,8 @@
 import ts from 'typescript';
-import { findReturnNodes, getFileContentFromSource } from './utilities';
-
-enum TemplateType {
-  FILE_REF = 0,
-  TEMPLATE_STRING = 1,
-}
-
-type TemplateDefinition = {
-  type: TemplateType;
-  content: string;
-};
-
-type ComponentInfo = {
-  name: string;
-  templates: Array<TemplateDefinition>;
-};
+import { extractComponentNodeFromAngularDeclaration } from './ng1-components/ng1-utilities';
 
 const scriptArguments = process.argv.slice(2);
 const filePath = scriptArguments[0];
-
-function getComponentNameIfComponent(node: ts.Node, source: ts.SourceFile) {
-  const nodeChildren = node.getChildren(source);
-
-  const argument = nodeChildren
-    .find(child => child.kind === ts.SyntaxKind.SyntaxList)
-    ?.getChildren(source)
-    .find(
-      child => child.kind === ts.SyntaxKind.StringLiteral
-    ) as ts.StringLiteral;
-
-  const identifier = nodeChildren
-    .find(child => child.kind === ts.SyntaxKind.PropertyAccessExpression)
-    ?.getChildren(source)
-    .find(child => child.kind === ts.SyntaxKind.Identifier) as ts.Identifier;
-
-  return identifier.escapedText === 'component'
-    ? argument.getText(source)
-    : null;
-}
-
-function findTemplatePathFromImport(importName: string, source: ts.SourceFile) {
-  const content = getFileContentFromSource(source);
-  const imports = content
-    .getChildren(source)
-    .filter(child => child.kind === ts.SyntaxKind.ImportDeclaration)
-    .flatMap(child => child.getChildren(source))
-    .reduce();
-  // group imports and file paths
-}
-
-function extractTemplateFromAngularDeclaration(
-  node: ts.Node,
-  source: ts.SourceFile
-) {
-  return node
-    .getChildren(source)
-    .filter(child => child.kind === ts.SyntaxKind.ObjectLiteralExpression)
-    .flatMap(objectLiteralExpression =>
-      objectLiteralExpression.getChildren(source)
-    )
-    .filter(child => child.kind === ts.SyntaxKind.PropertyAssignment)
-    .reduce((templates: Array<TemplateDefinition>, propertyAssignment) => {
-      const children = propertyAssignment.getChildren(source);
-      if (
-        children[0].kind === ts.SyntaxKind.Identifier &&
-        (children[0] as ts.Identifier).escapedText === 'template'
-      ) {
-        const templateType = children[1].kind;
-        switch (templateType) {
-          case ts.SyntaxKind.Identifier:
-            // Build template file path based on import name and file being processed
-            templates.push({
-              type: TemplateType.FILE_REF,
-              content: findTemplatePathFromImport(
-                (children[1] as ts.Identifier).escapedText,
-                source
-              ),
-            });
-            break;
-          case ts.SyntaxKind.ArrayLiteralExpression:
-            // Find returns and for each build a template
-            findReturnNodes(children[1], source).forEach(returnNode => {
-              const identifier = returnNode.getChildren()[0] as ts.Identifier;
-              templates.push({
-                type: TemplateType.FILE_REF,
-                content: findTemplatePathFromImport(
-                  identifier.escapedText,
-                  source
-                ),
-              });
-            });
-            break;
-          default:
-            templates.push({
-              type: TemplateType.TEMPLATE_STRING,
-              content: children[1].getFullText(),
-            });
-            break;
-        }
-      }
-      return templates;
-    }, []);
-}
-
-function extractComponentNodeFromAngularDeclaration(source: ts.SourceFile) {
-  const content = getFileContentFromSource(source);
-
-  const componentsDeclarations = content
-    .getChildren(source)
-    .filter(node => node.kind === ts.SyntaxKind.ExpressionStatement)
-    .flatMap(node => node.getChildren(source))
-    .filter(node => node.kind === ts.SyntaxKind.CallExpression);
-
-  const components = componentsDeclarations.reduce((componentsNode, node) => {
-    const name = getComponentNameIfComponent(node, source);
-
-    if (name) {
-      const templates = extractTemplateFromAngularDeclaration(node, source);
-      componentsNode.push({ name, templates });
-    }
-
-    return componentsNode;
-  }, [] as ComponentInfo[]);
-
-  console.log(components);
-}
 
 function main() {
   try {
@@ -132,7 +10,8 @@ function main() {
     const source = program.getSourceFile(filePath);
     if (!source) throw new Error(`Cannot find file ${filePath}`);
 
-    extractComponentNodeFromAngularDeclaration(source);
+    const componentsInfo = extractComponentNodeFromAngularDeclaration(source);
+    console.log(JSON.stringify(componentsInfo));
   } catch (e) {
     console.error(e);
   }
---
+++ ng1-components/ng1-utilities.ts
@@ -0,0 +1,166 @@
+import ts from 'typescript';
+import { findReturnNodes, getFileContentFromSource } from '../utilities/ast';
+
+enum TemplateType {
+  FILE_REF = 0,
+  TEMPLATE_STRING = 1,
+}
+
+type TemplateDefinition = {
+  type: TemplateType;
+  content?: string;
+};
+
+type ComponentInfo = {
+  name: string;
+  templates: Array<TemplateDefinition>;
+};
+
+export function getComponentNameIfComponent(
+  node: ts.Node,
+  source: ts.SourceFile
+): string | null {
+  const nodeChildren = node.getChildren(source);
+
+  const argument = nodeChildren
+    .find(child => child.kind === ts.SyntaxKind.SyntaxList)
+    ?.getChildren(source)
+    .find(
+      child => child.kind === ts.SyntaxKind.StringLiteral
+    ) as ts.StringLiteral;
+
+  const identifier = nodeChildren
+    .find(child => child.kind === ts.SyntaxKind.PropertyAccessExpression)
+    ?.getChildren(source)
+    .find(child => child.kind === ts.SyntaxKind.Identifier) as ts.Identifier;
+
+  return identifier.getText(source) === 'component'
+    ? argument.getText(source).slice(1, -1)
+    : null;
+}
+
+export function findTemplatePathFromImport(
+  importName: string,
+  source: ts.SourceFile
+): string | undefined {
+  const content = getFileContentFromSource(source);
+  const imports = content
+    .getChildren(source)
+    .filter(child => child.kind === ts.SyntaxKind.ImportDeclaration)
+    .map(importDeclaration => {
+      const parts = importDeclaration.getChildren(source);
+      const identifier = parts
+        .find(part => part.kind === ts.SyntaxKind.ImportClause)
+        ?.getText(source);
+      const path = parts
+        .find(part => part.kind === ts.SyntaxKind.StringLiteral)
+        ?.getText(source)
+        .replaceAll("'", '');
+      return { identifier, path };
+    });
+  return imports.find(fileImport => fileImport.identifier === importName)?.path;
+}
+
+export function extractTemplateFromAngularDeclaration(
+  node: ts.Node,
+  source: ts.SourceFile
+): TemplateDefinition[] {
+  const objectProperties = node
+    .getChildren(source)
+    .filter(child => child.kind === ts.SyntaxKind.SyntaxList)
+    .flatMap(child => child.getChildren(source))
+    .filter(child => child.kind === ts.SyntaxKind.ObjectLiteralExpression)
+    .flatMap(objectLiteralExpression =>
+      objectLiteralExpression.getChildren(source)
+    )
+    .filter(child => child.kind === ts.SyntaxKind.SyntaxList)
+    .flatMap(child => child.getChildren(source))
+    .filter(child => child.kind === ts.SyntaxKind.PropertyAssignment);
+
+  return objectProperties.reduce(
+    (templates: Array<TemplateDefinition>, propertyAssignment) => {
+      const children = propertyAssignment.getChildren(source);
+      const propertyIdentifier = children[0];
+
+      if (propertyIdentifier?.getText(source) !== 'template') return templates;
+      const templateValue = children
+        .slice(1)
+        .find(child =>
+          [
+            ts.SyntaxKind.Identifier,
+            ts.SyntaxKind.ArrayLiteralExpression,
+            ts.SyntaxKind.NoSubstitutionTemplateLiteral,
+            ts.SyntaxKind.TemplateExpression,
+          ].includes(child.kind)
+        );
+
+      if (!templateValue) return templates;
+      const templateType = templateValue?.kind;
+      switch (templateType) {
+        case ts.SyntaxKind.Identifier:
+          // Build template file path based on import name and file being processed
+          templates.push({
+            type: TemplateType.FILE_REF,
+            content: findTemplatePathFromImport(
+              (templateValue as ts.Identifier).getText(source),
+              source
+            ),
+          });
+          break;
+        case ts.SyntaxKind.ArrayLiteralExpression:
+          // Find returns and for each build a template
+          findReturnNodes(templateValue, source).forEach(returnNode => {
+            const returnIdentifier = returnNode
+              .getChildren(source)
+              .find(child => child.kind === ts.SyntaxKind.Identifier);
+            if (returnIdentifier) {
+              templates.push({
+                type: TemplateType.FILE_REF,
+                content: findTemplatePathFromImport(
+                  returnIdentifier.getText(source),
+                  source
+                ),
+              });
+            }
+          });
+          break;
+        case ts.SyntaxKind.NoSubstitutionTemplateLiteral:
+        case ts.SyntaxKind.TemplateExpression:
+          templates.push({
+            type: TemplateType.TEMPLATE_STRING,
+            content: templateValue.getText(source).slice(1, -1),
+          });
+          break;
+        default:
+          console.error('Unknown kind');
+      }
+      return templates;
+    },
+    []
+  );
+}
+
+export function extractComponentNodeFromAngularDeclaration(
+  source: ts.SourceFile
+): ComponentInfo[] {
+  const content = getFileContentFromSource(source);
+
+  const componentsDeclarations = content
+    .getChildren(source)
+    .filter(node => node.kind === ts.SyntaxKind.ExpressionStatement)
+    .flatMap(node => node.getChildren(source))
+    .filter(node => node.kind === ts.SyntaxKind.CallExpression);
+
+  const components = componentsDeclarations.reduce((componentsNode, node) => {
+    const name = getComponentNameIfComponent(node, source);
+
+    if (name) {
+      const templates = extractTemplateFromAngularDeclaration(node, source);
+      componentsNode.push({ name, templates });
+    }
+
+    return componentsNode;
+  }, [] as ComponentInfo[]);
+
+  return components;
+}
