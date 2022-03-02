59f560cf9c64e1ad443913aefc5d18d65d5ce290
Simon Bats <simon.bats12@gmail.com>
2022-03-02 10:52:46 -0500

    Create helper node finding methods

--- utilities/ast.ts
+++ utilities/ast.ts
@@ -31,3 +31,58 @@ export function findReturnNodes(
     sourceFile
   ) as Array<ts.ReturnStatement>;
 }
+
+export function findCallExpressions(
+  node: ts.Node,
+  sourceFile?: ts.SourceFile
+): Array<ts.CallExpression> {
+  return findNodesOfKind(
+    node,
+    ts.SyntaxKind.CallExpression,
+    sourceFile
+  ) as Array<ts.CallExpression>;
+}
+
+export function findPropertyAssignments(
+  node: ts.Node,
+  sourceFile?: ts.SourceFile
+): Array<ts.PropertyAssignment> {
+  return findNodesOfKind(
+    node,
+    ts.SyntaxKind.PropertyAssignment,
+    sourceFile
+  ) as Array<ts.PropertyAssignment>;
+}
+
+export function findObjectLiteralExpressions(
+  node: ts.Node,
+  sourceFile?: ts.SourceFile
+): Array<ts.ObjectLiteralExpression> {
+  return findNodesOfKind(
+    node,
+    ts.SyntaxKind.ObjectLiteralExpression,
+    sourceFile
+  ) as Array<ts.ObjectLiteralExpression>;
+}
+
+export function findIdentifiers(
+  node: ts.Node,
+  sourceFile?: ts.SourceFile
+): Array<ts.Identifier> {
+  return findNodesOfKind(
+    node,
+    ts.SyntaxKind.Identifier,
+    sourceFile
+  ) as Array<ts.Identifier>;
+}
+
+export function findImportDeclarations(
+  node: ts.Node,
+  sourceFile?: ts.SourceFile
+): Array<ts.ImportDeclaration> {
+  return findNodesOfKind(
+    node,
+    ts.SyntaxKind.ImportDeclaration,
+    sourceFile
+  ) as Array<ts.ImportDeclaration>;
+}
