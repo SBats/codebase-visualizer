6443a53b53e7452fb521368bc6745bd401a2be7f
Simon Bats <simon.bats12@gmail.com>
2022-03-05 22:33:36 -0500

    Fix tests setup

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,15 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
+  transform: {},
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 };
