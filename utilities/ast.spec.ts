02c1dc9ddefa9df317501682a4a7b4ee1ee9da76
Simon Bats <simon.bats12@gmail.com>
2022-03-05 22:35:16 -0500

    Fix findNodesOfKind tests

--- utilities/ast.spec.ts
+++ utilities/ast.spec.ts
@@ -2,14 +2,14 @@ import ts from 'typescript';
 import { findNodesOfKind, getFileContentFromSource } from './ast';
 import multiReturnFixture from '../fixtures/multi-return';
 
-test('Find 1 Block statement', () => {
+test('Find 4 Block statements', () => {
   const sourceFile = ts.createSourceFile(
     'MultiReturnTestFile',
     multiReturnFixture,
     ts.ScriptTarget.ESNext
   );
   const node = getFileContentFromSource(sourceFile);
-  expect(findNodesOfKind(node, ts.SyntaxKind.Block, sourceFile).length).toBe(1);
+  expect(findNodesOfKind(node, ts.SyntaxKind.Block, sourceFile).length).toBe(4);
 });
 
 test('Find 2 Return statements', () => {
